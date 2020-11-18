2067a9ed52bd83f0283a464c9dac801b3a7f854a
LaurensKubat <laurens.kubat@gmail.com>
2020-11-18 15:26:06 +0100

    add kernel module to read cr4

---
+++ assignment2/readRegister.c
@@ -0,0 +1,23 @@
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/init.h>      // included for __init and __exit macros
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Laurens");
+MODULE_DESCRIPTION("A Simple Hello World module");
+
+static int __init hello_init(void)
+{
+    register int i asm("cr4");
+    printk(KERN_INFO "Hello world!\n");
+    printk(i);
+    return 0;    // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit hello_cleanup(void)
+{
+    printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
